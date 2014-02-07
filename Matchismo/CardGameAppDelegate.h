7017f65c7777b0411674bb80078e61629234c7e6
tobolkac <tobolkac@gmail.com>
2014-02-07 11:55:37 -0600

    Initial Commit

---
+++ Matchismo/CardGameAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CardGameAppDelegate.h
+//  Matchismo
+//
+//  Created by Clay Tobolka on 2/7/14.
+//  Copyright (c) 2014 Clay Tobolka. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CardGameAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Matchismo/CardGameViewController.h
@@ -0,0 +1,13 @@
+//
+//  CardGameViewController.h
+//  Matchismo
+//
+//  Created by Clay Tobolka on 2/7/14.
+//  Copyright (c) 2014 Clay Tobolka. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CardGameViewController : UIViewController
+
+@end
